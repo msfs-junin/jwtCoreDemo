3657eddf145ca2c5a2b84317db763958cf90e730
ssabato <sebastian.sabato@cpba.com.ar>
2021-06-05 09:00:29 -0300

    Importo archivos

---
+++ jwtCoreDemo/jwtCoreDemo/Models/Configuration/JwtConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jwtCoreDemo.Models.Configuration
+{
+    public class JwtConfig
+    {
+        public string Key { get; set; }
+        public string Issuer{ get; set; }
+        public string Audience{ get; set; }
+        public int AccessTokenExpiration{ get; set; }
+    }
+}
